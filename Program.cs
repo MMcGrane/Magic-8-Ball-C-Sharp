b7598deb2d649a2225f9a7de9910bce70f076807
MMcGrane <markmcgrane@hotmail.com>
2020-07-26 19:32:01 +0100

    First up!
    The Magic 8 Ball!

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Magic_8_Ball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random r = new Random();
+
+            string[] answers = { "Yes", "No", "Not a chance!", "The Stars say No", "The Stars say yes",
+            "Maybe", "I don't know!", "Better not tell you.", "Without a doubt", "Very doubtful", 
+            "You may rely on it", "Ask yourself!"
+            };
+
+
+            Console.WriteLine("Welcome To Magic 8 Ball! \n");
+
+            Console.WriteLine("Please ask your question! \n");
+            string question = Console.ReadLine();
+
+            Console.WriteLine("You asked " + question + "\n");
+            Console.WriteLine(answers[r.Next(0, answers.Length)]);
+        }
+    }
+}
